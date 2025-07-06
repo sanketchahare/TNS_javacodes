255a844e2dcb3dc0c76eac5e1dfc266dc75a5596
chaha <chaha@LAPTOP-MQ91K5C0>
2025-07-07 01:42:44 +0530

    TNSjava

---
+++ DAY1/src/COM/DAY1/Firstprogram.java
@@ -0,0 +1,13 @@
+package COM.DAY1;
+
+public class Firstprogram {
+	public static void main(String[] args) {
+		System.out.println("hello" + "world");
+		System.out.println(2 + 4);
+		
+		
+		
+		
+	}
+
+}
